e139a15d96a2fbf8957bcb550257ff9216ff2cba
JoGarcia26 <jogarcia@etrr.edu.ar>
2024-09-27 08:58:41 -0300

    Update Heroes y Villanos Codigo.c

--- Heroes y Villanos Codigo.c
+++ Heroes y Villanos Codigo.c
@@ -31,10 +31,10 @@ void imprimirPersonaje(struct Personaje p){
     printf("Defensa que tiene: %d, %d y %d\n", p.defensa[0], p.defensa[1], p.defensa[2]);
 }
 int main() {
-    int danioAlmighty[3] = {30, 40, 50};
+    int danioAlmighty[3] = {30, 40, 70};
     int defensaAlmighty[3] = {50, 60, 70};
-    int danioEvilBunny[3] = {25, 35, 45};
-    int defensaEvilBunny[3] = {55, 65, 75};
+    int danioEvilBunny[3] = {15, 55, 70};
+    int defensaEvilBunny[3] = {35, 65, 75};
 
     struct Personaje Almighty = crearPersonaje("Almighty", "Héroe y cantante que combate a Evil Bunny, quien quiere controlar el mundo con ideas obscenas", VIDA_INICIO, danioAlmighty, defensaAlmighty);
     struct Personaje EvilBunny = crearPersonaje("Evil Bunny", "Villano que quiere controlar el mundo con sus ideas obscenas", VIDA_INICIO, danioEvilBunny, defensaEvilBunny);
